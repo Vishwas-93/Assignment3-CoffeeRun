4e74331fa233c423b86d5d4180084ed90b3edd7b
Vishwas-93 <vishwas.vasudevamanja@gmail.com>
2017-09-25 21:38:30 -0700

    Delete test_truck.js

--- CoffeeRun/coffeerun/tests/test_truck.js
+++
@@ -1,7 +0,0 @@
-myTruck.createOrder({ emailAddress: 'me@goldfinger.com', coffee: 'double mocha'});
-myTruck.createOrder({ emailAddress: 'dr@no.com', coffee: 'decaf'});
-myTruck.createOrder({ emailAddress: 'm@bond.com', coffee: 'earl grey'});
-myTruck.printOrders();
-myTruck.deliverOrder('dr@no.com');
-myTruck.deliverOrder('m@bond.com');
-myTruck.printOrders();
